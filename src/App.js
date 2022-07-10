8a68639d1969132cb1d6f253d4f14b41e4dd3051
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-10 13:23:29 -0300

    feat: navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import { Navbar } from './components';
 
 const App = () => (
   <div className="app">
-    
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main" />
+    <div className="footer" />
   </div>
 );
 
