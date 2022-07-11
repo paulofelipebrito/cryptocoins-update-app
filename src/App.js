979ae5b0b933a09e6a3cd764d4ba4719c9c6f83d
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-11 07:55:55 -0300

    feat: Homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 import './App.css';
-import { Navbar } from './components';
+import { Exchanges, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 
 const App = () => (
   <div className="app">
     <div className="navbar">
       <Navbar />
     </div>
-    <div className="main" />
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+    </div>
     <div className="footer" />
   </div>
 );
