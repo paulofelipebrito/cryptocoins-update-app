a6d210d9e2ea4062741b8d8018a1a3f76fd32b76
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-09-14 08:10:12 -0300

    fix: api typo

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -20,7 +20,7 @@ export const cryptoApi = createApi({
     }),
 
     getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timePeriod=${timeperiod}`),
     }),
 
     // Note: To access this endpoint you need premium plan
